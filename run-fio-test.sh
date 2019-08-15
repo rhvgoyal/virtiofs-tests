7380171967b17a99d9895cace9cd1af3aa1d79c2
Vivek Goyal <vgoyal@redhat.com>
2019-08-15 13:02:34 -0400

    capture some system info while running tests
    This info can be useful to figure out system config.
    
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>

--- run-fio-test.sh
+++ run-fio-test.sh
@@ -7,6 +7,24 @@ drop_cache() {
   echo 3 > /proc/sys/vm/drop_caches
 }
 
+get_shm_regions() {
+  local regions
+
+  regions=`cat /proc/iomem | grep "virtio-pci-shm" | sed 's/^ *//' | cut -d " " -f1 | tr '\n' ' '`
+  echo $regions
+}
+
+# Print metadata about the job
+output_meta_info() {
+  echo "TESTDIR=$TESTDIR"
+  echo "JOB_FILES=$JOB_FILES"
+  echo "TEST_NAME=$TEST_NAME"
+  echo "SYSTEMINFO=`uname -a`"
+  echo "MOUNTPOINT=`findmnt -n --target $TESTDIR`"
+  echo "VIRTO_SHM_REGIONS=`get_shm_regions`"
+  echo ""
+}
+
 run_test() {
   drop_cache
   fio --directory=$TESTDIR --append-terse $1
@@ -41,7 +59,6 @@ fi
 
 shift 2
 JOB_FILES="$@"
-echo "TESTDIR=$TESTDIR JOB_FILES=$JOB_FILES"
 
 if [ -z "$JOB_FILES" ];then
   echo "Specify atleast one job file"
@@ -49,8 +66,8 @@ if [ -z "$JOB_FILES" ];then
   exit 1
 fi
 
+output_meta_info
 drop_cache
-echo "TEST_NAME=$TEST_NAME"
 for jobname in $JOB_FILES;do
   for i in `seq 1 $NR_RUN`;do
      run_test $jobname
